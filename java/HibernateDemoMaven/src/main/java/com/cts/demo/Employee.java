55c531cea46a89de3ad28d3eb75270ec556dfcbc
puneet <puneetvashsiht@gmail.com>
2017-05-24 11:12:51 +0530

    java

---
+++ java/HibernateDemoMaven/src/main/java/com/cts/demo/Employee.java
@@ -0,0 +1,32 @@
+package com.cts.demo;
+
+public class Employee {
+	
+	int id;
+	String name;
+	public Employee(){
+		
+	}
+	public Employee(String name) {
+		super();
+//		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
