f0b25e9b47dd3da74bfc5c776ef17807cdf3e163
puneet <puneetvashsiht@gmail.com>
2017-06-01 12:21:30 +0530

    final

--- BaseCodeSlice_MyAssessmentApplication/src/com/cognizant/controller/EmployeeController.java
+++ BaseCodeSlice_MyAssessmentApplication/src/com/cognizant/controller/EmployeeController.java
@@ -1,125 +1,62 @@
 package com.cognizant.controller;
 
+
+
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cognizant.entity.Employee;
-import com.cognizant.exception.ApplicationException;
 import com.cognizant.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
+	
 	@Autowired
 	EmployeeService service;
 	private List<Employee> employees;
 	
-	public EmployeeController() {
-		super();
-	}
-
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
-	public String addMember(Model model){
-		model.addAttribute("m1", "Kumar");
-		return "member";		
-	}
-
-	@RequestMapping(value="/add", method=RequestMethod.GET)
-	public String addMember(@RequestParam("name") String name, Model model){
-		employees = new ArrayList<Employee>();
-		Employee emp = new Employee(name, 3434.2);
-		employees.add(emp);
-		try {
-			service.addEmployees(employees);
-			model.addAttribute("m1", name);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return "member";
-	}
-	
-//	http://localhost:8080/BaseCodeSlice_BankManagementSystem/mvc/add2?name=jagga
-	@RequestMapping(value="/add2", method=RequestMethod.GET)
-	public String addEmp(
-			@RequestParam("name") String name, 
-//			@RequestParam("email") String email, 
-//			@RequestParam("salary") double salary, 
-//			@RequestParam("age") int age, 
-//			@RequestParam("joiningDate") Date joiningDate, 
-
-			Model model){
-		employees = new ArrayList<Employee>();
+	@RequestMapping(value="/employee", method=RequestMethod.GET)
+	public String getEmployee(@ModelAttribute("employee") @Valid Employee employee,
+			BindingResult result, Model model){
 		
-		String email = "jagga@gmail.com";
-		double salary = 360000.0;
-		int age = 25;
-
-		Employee emp = new Employee();
-		emp.setName(name);
-		emp.setEmail(email);
-		emp.setSalary(salary);
-		emp.setAge(age);
-
-		employees.add(emp);
-		try {
-			service.addEmployees(employees);
-			model.addAttribute("m1", name);
-		} catch (javax.validation.ConstraintViolationException e) {
-			Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
-			Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
-			while(iterator.hasNext()){
-				ConstraintViolation<?> next = iterator.next();
-				System.out.println("Validation message: "+next.getMessage());
-				System.out.println("Invalid field: "+next.getPropertyPath());
-				System.out.println("Validation class/bean: "+next.getRootBean());
-			}
-			e.printStackTrace();
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-
-		return "member";
+		return "employee";
 	}
 	
 	
-}
+	@RequestMapping(value="/employees",method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody  List<Employee> getEmployees(@RequestParam("name") String name){
+		
+		Employee emp1 = new Employee(name, 34344.44);
+		Employee emp2 = new Employee("Priya", 35455.00);
+		employees = new ArrayList<>();
+		employees.add(emp1);
+		employees.add(emp2);
+		return employees;
+	}
+	
 
-
-/*
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.cognizant.entity.Employee;
-import com.cognizant.service.EmployeeService;
-
-
-@Controller
-public class EmployeeController {
-
-	@Autowired
-	EmployeeService service;
-	private List<Employee> employees;
-
+	
+	
 	@RequestMapping(value="/add", method=RequestMethod.GET)
 	public String addMember(@RequestParam("name") String name, Model model){
-		employees = new ArrayList<>();
+		employees = new ArrayList<>();	
 		Employee emp = new Employee(name, 3434.2);
 		employees.add(emp);
 		try {
@@ -129,9 +66,58 @@ public class EmployeeController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 		return "member";
+	}
+	
+	@RequestMapping(value = "/employee", method = RequestMethod.POST)
+	public String saveOrUpdateEmployee(
+		@ModelAttribute("employee") @Valid Employee employee,
+		BindingResult result, 
+		Model model) {
+		
+		System.out.println(employee);
+		employees = new ArrayList<>();	
+		employees.add(employee);
+		
+//		EmployeeValidator validator = new EmployeeValidator();
+		
+//		 Set<ConstraintViolation<Object>> constraintViolations = validator.validate(employee, result);
+		 
+		        
+
+		
+//		new EmployeeValidator().validate(employee, result);
+		try {
+			service.addEmployees(employees);
+		} 
+		catch (ConstraintViolationException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+			Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+			Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
+			while(iterator.hasNext()){
+				ConstraintViolation<?> next = iterator.next();
+				System.out.println("Validation message: "+next.getMessage());
+				System.out.println("Invalid field: "+next.getPropertyPath());
+				System.out.println("Validation class/bean: "+next.getRootBean());
+//				 result.reject(next.getPropertyPath(), next.getMessage());
+				result.rejectValue(next.getPropertyPath().toString(), "", next.getMessage());
+//				result.rejectValue(arg0, arg1, arg2);
+			}
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("m1", employee.getName());
+
+		if (result.hasErrors()) {
+			return "employee";
+		} 
+		
+		return "member";
+
 	}
 
 }
-*/
--- BaseCodeSlice_MyAssessmentApplication/src/com/cognizant/entity/Employee.java
+++ BaseCodeSlice_MyAssessmentApplication/src/com/cognizant/entity/Employee.java
@@ -4,30 +4,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
 @Entity
 public class Employee {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+//	
+//	@javax.validation.constraints.
+	@NotNull
+    @Size(min=2, max=30)
+	private String name;
 	
-	@Size(min=5, max=12) 
-    private String name;
-     
-    @NotEmpty @Email
-    private String email;
-     
-    @NotNull @Min(18) @Max(70)
-    private Integer age;
-     
-	private Double salary;
+	@Min(value=5000)
+	private double salary;
 	
 	public Employee(){
 		
@@ -37,6 +31,18 @@ public class Employee {
 		super();
 		this.name = name;
 		this.salary = salary;
+	}
+	
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
 	}
 
 	public int getId() {
@@ -55,34 +61,11 @@ public class Employee {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public Double getSalary() {
+	public double getSalary() {
 		return salary;
 	}
 
-	public void setSalary(Double salary) {
+	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", email=" + email
-				+ ", age=" + age + ", salary=" + salary + "]";
-	}
-	
 }
